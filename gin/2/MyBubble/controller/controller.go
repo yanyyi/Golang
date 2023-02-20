d07e883d1d6a83f62a78ffde45799baef04b38ab
Your Name <you@example.com>
2023-02-20 11:31:48 +0800

    finish go_web_2

---
+++ gin/2/MyBubble/controller/controller.go
@@ -0,0 +1,77 @@
+package controller
+
+import (
+	"MyBubble/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func IndexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
+}
+
+func CreateTodo(c *gin.Context) {
+	//前端页面填写待办提交 点击提交 会发请求到这里
+	// 1.从请求中把数据拿出来
+	var todo models.Todo
+	c.BindJSON(&todo)
+	err := models.CreateATodo(&todo)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"erroe": err.Error()})
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+
+}
+
+func GetTodoList(c *gin.Context) {
+	todoList, err := models.GetAllTodo()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, todoList)
+	}
+}
+
+func UpdateATodo(c *gin.Context) {
+	id, ok := c.Params.Get("id")
+	if !ok {
+		c.JSON(http.StatusOK, gin.H{
+			"error": "无效的id",
+		})
+		return
+	}
+	todo, err := models.GetATodo(id)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+	}
+	c.BindJSON(&todo)
+	if err = models.Update(todo); err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+}
+
+func DeleteATodo(c *gin.Context) {
+
+	id, ok := c.Params.Get("id")
+	if !ok {
+		c.JSON(http.StatusOK, gin.H{
+			"error": "无效的id",
+		})
+	}
+	if err := models.DeleteATodo(id); err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			id: "deleted",
+		})
+	}
+}
