e971c520ac5bd5414930284cbcd56f39d75ed51d
Your Name <you@example.com>
2023-02-21 13:40:13 +0800

    finish stack code

--- data_structure/stacks/main.go
+++ data_structure/stacks/main.go
@@ -12,6 +12,7 @@ type Stack struct {
 	arr    [5]int //数组模拟栈
 }
 
+// 入栈
 func (this *Stack) Push(val int) (err error) {
 	//先判断栈是否满了
 	if this.Top == this.MaxTop-1 {
@@ -26,9 +27,56 @@ func (this *Stack) Push(val int) (err error) {
 
 // 遍历栈,注意需要从栈顶开始遍历
 func (this *Stack) List() {
+	//先判断栈是否为空
+	if this.Top == -1 {
+		fmt.Println("stack empty")
+		return
+	}
+	fmt.Println("栈的情况如下:")
+	for i := this.Top; i >= 0; i-- {
+		fmt.Printf("arr[%d]=%d\n", i, this.arr[i])
+	}
+}
 
+// 出栈
+func (this *Stack) Pop() (val int, err error) {
+	//判断栈是否为空
+	if this.Top == -1 {
+		fmt.Println("stack empty")
+		return 0, errors.New("stack empty")
+	}
+	//先取值,再 this.Top--
+	val = this.arr[this.Top]
+	this.Top--
+	return val, nil
 }
 
 func main() {
-	fmt.Println("hello stack")
+	stack := &Stack{
+		MaxTop: 5,
+		Top:    -1,
+	}
+	//入栈
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+	stack.Push(5)
+	stack.Push(6) //报错: stack full
+	//显示
+	stack.List()
+
+	//出栈
+	val, _ := stack.Pop()
+	fmt.Println("出栈val=", val)
+	val, _ = stack.Pop()
+	fmt.Println("出栈val=", val)
+
+	stack.List()
+	val, _ = stack.Pop()
+	val, _ = stack.Pop()
+	val, _ = stack.Pop()
+	val, _ = stack.Pop() //报错: stack empty
+	fmt.Println("出栈val=", val)
+
 }
