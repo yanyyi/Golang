ce89c009cc4ccbb35dda822e5de691322313bd65
Your Name <you@example.com>
2023-02-21 13:17:22 +0800

    finish gorm curd

--- orm/gorm/liwenzhou/gorm_curd/main.go
+++ orm/gorm/liwenzhou/gorm_curd/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 type User struct {
-	ID   int64
+	gorm.Model
 	Name string
 	Age  int64
 }
@@ -22,8 +22,35 @@ func main() {
 	db.AutoMigrate(&User{})
 
 	//3.创建
-	u := User{Name: "qimi", Age: 18} //在代码层面创建一个User对象
-	//fmt.Println(db.NewRecord(&u))    //判断主键是否为空,相当于查看表中是否有数据 此处返回true
-	db.Create(&u)
-	//fmt.Println(db.NewRecord(&u)) //  此处返回false
+	//u := User{Name: "qimi", Age: 18} //在代码层面创建一个User对象
+	////fmt.Println(db.NewRecord(&u))    //判断主键是否为空,相当于查看表中是否有数据 此处返回true
+	//db.Create(&u)
+	////fmt.Println(db.NewRecord(&u)) //  此处返回false
+	//u2 := User{Name: "Stephen", Age: 25}
+	//db.Create(&u2)
+
+	//4.查询
+	//user := new(User)
+	//db.First(&user)
+	//fmt.Printf("user: %s\n", *user)
+
+	//var users []User
+	//db.Find(&users)
+	//fmt.Printf("users: %s\n", users)
+
+	//5.更新
+	//db.Debug().Model(&user).Where("name=?", "qimi").Update("name", "John")
+	//db.Debug().Model(&user).Where("name=?", "John").Update(map[string]interface{}{"name": "Chen", "age": 50})
+	//
+	////只更新map中的某些字段
+	//conitionMap := map[string]interface{}{
+	//	"name": "Stephen",
+	//	"age":  20,
+	//}
+	//db.Debug().Model(&user).Select("age").Update(conitionMap) //只更新age字段
+	//db.Debug().Model(&user).Omit("age").Update(conitionMap)   //忽略age字段更新其他字段
+
+	//6.删除
+	db.Debug().Unscoped().Where("name=?", "Stephen").Delete(&User{})
+
 }
