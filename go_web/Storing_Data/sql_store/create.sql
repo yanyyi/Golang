faf9a82db1f049fc42ddf246b1c94d606bfd0418
Your Name <you@example.com>
2023-03-05 01:19:55 +0800

    change go_web location

--- go_web/Storing_Data/sql_store/create.sql
+++
@@ -1,8 +0,0 @@
-create database gwp;
-
-use gwp;
-create table posts(
-                      id  int  primary key auto_increment,
-                      content varchar(255) ,
-                      author varchar(255)
-);
--- go_web/Storing_Data/sqlx_store/create.sql
+++
@@ -1,9 +0,0 @@
-use gwp;
-create table posts (
-   id      int auto_increment  primary key,
-   content varchar(255),
-   author  varchar(255)
-);
-
-insert into posts (content, author) values("Go语言","Google");
-insert into posts (content, author) values("Windows操作系统","Microsoft");
