51d254a161c62e11406dd8fb1f51830068806bc1
Stephen <Stephen@qq.com>
2023-03-16 17:25:51 +0800

    modify gin codes

---
+++ gin/MyBubble/routers/routers.go
@@ -0,0 +1,29 @@
+package routers
+
+import (
+	"MyBubble/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	r.Static("/static", "static")
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", controller.IndexHandler)
+	//v1
+	v1Group := r.Group("v1")
+	{
+		//待办事项
+
+		//添加
+		v1Group.POST("/todo", controller.CreateTodo)
+		//查看所有的待办事项
+		v1Group.GET("/todo", controller.GetTodoList)
+
+		//修改
+		v1Group.PUT("/todo/:id", controller.UpdateATodo)
+		//删除
+		v1Group.DELETE("/todo/:id", controller.DeleteATodo)
+	}
+	return r
+}
