7063062c5366202e7ebef734968b975d3b283143
Your Name <you@example.com>
2023-04-08 13:26:04 +0800

    add gee-web codes

---
+++ gee-web/day0-myWeb/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"Golang/gee-web/day0-myWeb/dragon"
+	"log"
+	"net/http"
+	"time"
+)
+
+func onlyForV2() dragon.HandlerFunc {
+	return func(c *dragon.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
+func main() {
+	r := dragon.New()
+	r.Use(dragon.Logger()) // global midlleware
+	r.GET("/", func(c *dragon.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
+	{
+		v2.GET("/hello/:name", func(c *dragon.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+	}
+
+	r.Run(":9999")
+}
